f260ea92186ac59ca3b48cab4e6b64a5f2402741
wodesuck <wodesuck@gmail.com>
2015-10-05 22:06:24 +0800

    replace this

--- web/js/chat.js
+++ web/js/chat.js
@@ -1,49 +1,46 @@
-var participants = {
+var pop = {
     count: {},
     list: [],
     load: function () {
         $.get("a/participants", {}, function (res) {
             if (res.err == 0) {
-                with (participants) {
-                    count = {};
-                    res.result.forEach(function (user) {
-                        if (!count.hasOwnProperty(user)) count[user] = 0;
-                        count[user]++;
-                    });
-                    list = [];
-                    for (var user in count) list.push(user);
-                    list.sort();
-                    var par = $("#participants");
-                    par.innerHTML = "";
-                    list.forEach(function (user) {
-                        var e = document.createElement("li");
-                        e.innerHTML = user;
-                        par.appendChild(e).show(100);
-                    });
-                }
+                pop.count = {};
+                res.result.forEach(function (user) {
+                    if (!pop.count.hasOwnProperty(user)) pop.count[user] = 0;
+                    pop.count[user]++;
+                });
+                pop.list = Object.getOwnPropertyNames(pop.count).sort();
+                pop.list.sort();
+                var par = $("#participants");
+                par.innerHTML = "";
+                pop.list.forEach(function (user) {
+                    var e = document.createElement("li");
+                    e.innerHTML = user;
+                    par.appendChild(e).show(100);
+                });
             }
         });
     },
     add: function (user) {
-        if (!this.count.hasOwnProperty(user)) {
-            this.count[user] = 1;
-            this.list.push(user);
-            this.list.sort();
-            var pos = this.list.indexOf(user);
+        if (!pop.count.hasOwnProperty(user)) {
+            pop.count[user] = 1;
+            pop.list.push(user);
+            pop.list.sort();
+            var pos = pop.list.indexOf(user);
             var e = document.createElement("li");
             e.innerHTML = user;
             var par = $("#participants");
             par.insertBefore(e, par.childNodes[pos]).show(100);
         } else {
-            this.count[user]++;
+            pop.count[user]++;
         }
     },
     remove: function (user) {
-        this.count[user]--;
-        if (this.count[user] == 0) {
-            delete this.count[user];
-            var pos = this.list.indexOf(user);
-            this.list.splice(pos, 1);
+        pop.count[user]--;
+        if (pop.count[user] == 0) {
+            delete pop.count[user];
+            var pos = pop.list.indexOf(user);
+            pop.list.splice(pos, 1);
             var e = $("#participants").childNodes[pos];
             e.hide(100, function () {
                 e.parentNode.removeChild(e);
@@ -55,24 +52,26 @@ var participants = {
 var chat = {
     connect: function () {
         var wsurl = "ws" + /:.*\//.exec(location) + "wschat";
-        this.ws = new WebSocket(wsurl);
-        this.ws.onopen = participants.load;
-        this.ws.onmessage = this.recv;
+        chat.ws = new WebSocket(wsurl);
+        chat.ws.onopen = pop.load;
+        chat.ws.onmessage = chat.recv;
     },
     recv: function (message) {
         message = JSON.parse(message.data);
         if (message.type == 1) {
-            participants.add(message.sender);
+            pop.add(message.sender);
         } else if (message.type == 2) {
-            participants.remove(message.sender);
+            pop.remove(message.sender);
         } else if (message.type == 3) {
             var e = document.createElement("p");
-            e.innerHTML = "<strong>" + message.sender + ": </strong>" + message.msg;
+            e.appendChild(document.createElement("strong"));
+            e.childNodes[0].innerHTML = message.sender + ": ";
+            e.innerHTML += message.msg;
             $("#chatlog").appendChild(e).show(100);
         }
     },
     send: function (message) {
-        this.ws.send(JSON.stringify({type: 3, msg: message}));
+        chat.ws.send(JSON.stringify({type: 3, msg: message}));
     }
 };
 
