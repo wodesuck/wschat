99d372f53bf2eb796d2f35341ea30e24dd6663d6
wodesuck <wodesuck@gmail.com>
2015-10-08 20:10:43 +0800

    jump to the wrong field when register/passwd failed

--- web/js/passwd.js
+++ web/js/passwd.js
@@ -2,42 +2,44 @@ function passwd() {
     $(".alert").hide(100);
     var oldpassword = $("input[name='oldpassword']").value;
     var password = $("input[name='newpassword']").value;
-	var passwordrepeat = $("input[name='newpasswordrepeat']").value;
-	if(oldpassword == "" || password == ""){
-		$("#msg").innerHTML = " 密码不能为空 ";
-		$(".alert").show(100);
-	}else if(password != passwordrepeat){
-		$("#msg").innerHTML = " 两次密码不一致 ";
-		$(".alert").show(100);
-	}else {
-		$.post("a/passwd", {oldpassword: oldpassword, password: password}, function (res) {
-			if (res.err == 0) {
-				location = /.*\//.exec(location) ;
-			} else {
-				if (res.err == -1) {
-					$("#msg").innerHTML = "请先登录";
-					$("input[type='password']").value = "";
-					$("input[type='password']").focus();
-				} else if (res.err == -2) {
-					$("#msg").innerHTML = "旧密码错误";
-					$("input[type='password']").value = "";
-					$("input[type='password']").focus();
-				} else if (res.err == -3) {
-					$("#msg").innerHTML = "更新失败";
-					$("input[type='password']").value = "";
-					$("input[type='password']").focus();
-				} else {
-					$("#msg").innerHTML = "未知错误";
-				}
-				$(".alert").show(100);
-			}
-		})
-	}
+    var passwordrepeat = $("input[name='newpasswordrepeat']").value;
+    if (oldpassword == "" || password == "") {
+        $("#msg").innerHTML = " 密码不能为空 ";
+        if (oldpassword == "") {
+            $("input[name='oldpassword']").focus();
+        } else {
+            $("input[name='newpassword']").focus();
+        }
+        $(".alert").show(100);
+    } else if (password != passwordrepeat) {
+        $("#msg").innerHTML = " 两次密码不一致 ";
+        $("input[name='newpassword']").value = "";
+        $("input[name='newpasswordrepeat']").value = "";
+        $("input[name='newpassword']").focus();
+        $(".alert").show(100);
+    } else {
+        $.post("a/passwd", {oldpassword: oldpassword, password: password}, function (res) {
+            if (res.err == 0) {
+                location = /.*\//.exec(location);
+            } else {
+                if (res.err == -1) {
+                    $("#msg").innerHTML = "请先登录";
+                } else if (res.err == -2) {
+                    $("#msg").innerHTML = "旧密码错误";
+                    $("input[name='oldpassword']").value = "";
+                    $("input[name='oldpassword']").focus();
+                } else {
+                    $("#msg").innerHTML = "未知错误";
+                }
+                $(".alert").show(100);
+            }
+        })
+    }
 }
 
 $.ready(function () {
     $(".alert").hide();
-    $("button").addEventListener("click", function(e) {
+    $("button").addEventListener("click", function (e) {
         e.preventDefault();
         passwd();
     });
--- web/js/register.js
+++ web/js/register.js
@@ -2,34 +2,42 @@ function register() {
     $(".alert").hide(100);
     var username = $("input[type='text']").value;
     var password = $("input[name='password']").value;
-	var passwordrepeat = $("input[name='passwordrepeat']").value;
-	if(username == "" || password == ""){
-		$("#msg").innerHTML = " 账号密码不能为空" ;
-		$(".alert").show(100);
-	}else if(password != passwordrepeat){
-		$("#msg").innerHTML = " 密码不一致" ;
-		$(".alert").show(100);
-	}else {
-		$.post("a/register", {username: username, password: password}, function (res) {
-			if (res.err == 0) {
-				location = /.*\//.exec(location) ;
-			} else {
-				if (res.err == -1) {
-					$("#msg").innerHTML = "用户已经被注册!";
-					$("input[type='text']").value = "";
-					$("input[type='text']").focus();
-				} else {
-					$("#msg").innerHTML = "未知错误";
-				}
-				$(".alert").show(100);
-			}
-		})
-	}
+    var passwordrepeat = $("input[name='passwordrepeat']").value;
+    if (username == "" || password == "") {
+        $("#msg").innerHTML = " 账号密码不能为空";
+        if (username == "") {
+            $("input[type='text']").focus();
+        } else {
+            $("input[name='password']").focus();
+        }
+        $(".alert").show(100);
+    } else if (password != passwordrepeat) {
+        $("#msg").innerHTML = " 密码不一致";
+        $("input[name='password']").value = "";
+        $("input[name='passwordrepeat']").value = "";
+        $("input[name='password']").focus();
+        $(".alert").show(100);
+    } else {
+        $.post("a/register", {username: username, password: password}, function (res) {
+            if (res.err == 0) {
+                location = /.*\//.exec(location);
+            } else {
+                if (res.err == -1) {
+                    $("#msg").innerHTML = "用户已经被注册!";
+                    $("input[type='text']").value = "";
+                    $("input[type='text']").focus();
+                } else {
+                    $("#msg").innerHTML = "未知错误";
+                }
+                $(".alert").show(100);
+            }
+        })
+    }
 }
 
 $.ready(function () {
     $(".alert").hide();
-    $("button").addEventListener("click", function(e) {
+    $("button").addEventListener("click", function (e) {
         e.preventDefault();
         register();
     });
